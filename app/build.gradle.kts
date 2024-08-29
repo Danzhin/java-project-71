7f953d78bb2901004292183d8d9ad3434771c661
dronz <dronzhin123@gmail.com>
2024-08-29 11:22:18 +0300

    4 stage

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("info.picocli:picocli:4.7.6")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
