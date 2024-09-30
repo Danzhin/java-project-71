3d08ec972dfcdb5597e5ac6a454c0e7bf30ea739
dronz <dronzhin123@gmail,com>
2024-09-30 12:34:32 +0300

    update

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,33 +9,22 @@ public class DifferTest {
     private static final String FIX_PATH = "src/test/resources/fixtures/";
 
     @Test
-    public void testGenerate1() throws Exception {
-        String expected = FileReader.readFile(FIX_PATH + "R1.txt");
+    public void testGenerate() throws Exception {
+        String expected1 = FileReader.readFile(FIX_PATH + "E1.txt");
+        String expected2 = FileReader.readFile(FIX_PATH + "E2.txt");
 
         String filePathA = FIX_PATH + "A1.json";
         String filePathB = FIX_PATH + "B1.json";
-        assertEquals(expected, Differ.generate(filePathA, filePathB, "stylish"));
+        assertEquals(expected1, Differ.generate(filePathA, filePathB, "stylish"));
+        assertEquals(expected2, Differ.generate(filePathA, filePathB, "plain"));
 
         filePathB = FIX_PATH + "B1.yaml";
-        assertEquals(expected, Differ.generate(filePathA, filePathB, "stylish"));
+        assertEquals(expected1, Differ.generate(filePathA, filePathB, "stylish"));
+        assertEquals(expected2, Differ.generate(filePathA, filePathB, "plain"));
 
         filePathA = FIX_PATH + "A1.yaml";
-        assertEquals(expected, Differ.generate(filePathA, filePathB, "stylish"));
-    }
-
-    @Test
-    public void testGenerate2() throws Exception {
-        String expected = FileReader.readFile(FIX_PATH + "R2.txt");
-
-        String filePathA = FIX_PATH + "A1.json";
-        String filePathB = FIX_PATH + "B1.json";
-        assertEquals(expected, Differ.generate(filePathA, filePathB, "plain"));
-
-        filePathB = FIX_PATH + "B1.yaml";
-        assertEquals(expected, Differ.generate(filePathA, filePathB, "plain"));
-
-        filePathA = FIX_PATH + "A1.yaml";
-        assertEquals(expected, Differ.generate(filePathA, filePathB, "plain"));
+        assertEquals(expected1, Differ.generate(filePathA, filePathB, "stylish"));
+        assertEquals(expected2, Differ.generate(filePathA, filePathB, "plain"));
     }
 
 }
