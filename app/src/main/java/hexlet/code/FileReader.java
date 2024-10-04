9c7bcfbf65513ca966f074291be1eabaf4219c18
dronz <dronzhin123@gmail,com>
2024-10-04 22:10:07 +0300

    update

--- app/src/main/java/hexlet/code/FileReader.java
+++ app/src/main/java/hexlet/code/FileReader.java
@@ -10,7 +10,6 @@ public class FileReader {
         return filePath.substring(filePath.lastIndexOf('.') + 1);
     }
 
-
     public static String readFile(String filePath) throws IOException {
         return Files.readString(Paths.get(filePath));
     }
