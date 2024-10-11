7b6c11e26e02909c02dbcf6849ab8117d5d5cb10
dronz <dronzhin123@gmail,com>
2024-10-11 14:30:41 +0300

    update

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -8,15 +9,15 @@ import java.util.Map;
 
 public class Parser {
 
-    public static Map<String, Object> toMap(String fileData, String extension) throws Exception {
+    public static Map<String, Object> toMap(String fileData, String extension) throws JsonProcessingException {
         return getObjectMapper(extension).readValue(fileData, new TypeReference<>() { });
     }
 
-    private static ObjectMapper getObjectMapper(String extension) throws Exception {
+    private static ObjectMapper getObjectMapper(String extension) {
         return switch (extension) {
             case "json" -> new ObjectMapper();
             case "yaml", "yml" -> new ObjectMapper(new YAMLFactory());
-            default -> throw new Exception("incorrect extension");
+            default -> throw new IllegalStateException("Unexpected value: " + extension);
         };
     }
 
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,48 +8,45 @@ public class DifferTest {
 
     private static final String FIX_PATH = "src/test/resources/fixtures/";
 
-    private static String expected;
-    private static String actual;
-
     @Test
     public void testGenerateJsonStylish() throws Exception {
-        expected = FileReader.readFile(FIX_PATH + "E1.txt");
-        actual =  Differ.generate(FIX_PATH + "A1.json", FIX_PATH + "B1.json", "stylish");
+        String expected = FileReader.readFile(FIX_PATH + "E1.txt");
+        String actual =  Differ.generate(FIX_PATH + "A1.json", FIX_PATH + "B1.json", "stylish");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateYamlStylish() throws Exception {
-        expected = FileReader.readFile(FIX_PATH + "E1.txt");
-        actual =  Differ.generate(FIX_PATH + "A1.yaml", FIX_PATH + "B1.yaml", "stylish");
+        String expected = FileReader.readFile(FIX_PATH + "E1.txt");
+        String actual =  Differ.generate(FIX_PATH + "A1.yaml", FIX_PATH + "B1.yaml", "stylish");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateJsonPlain() throws Exception {
-        expected = FileReader.readFile(FIX_PATH + "E2.txt");
-        actual =  Differ.generate(FIX_PATH + "A1.json", FIX_PATH + "B1.json", "plain");
+        String expected = FileReader.readFile(FIX_PATH + "E2.txt");
+        String actual =  Differ.generate(FIX_PATH + "A1.json", FIX_PATH + "B1.json", "plain");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateYamlPlain() throws Exception {
-        expected = FileReader.readFile(FIX_PATH + "E2.txt");
-        actual =  Differ.generate(FIX_PATH + "A1.yaml", FIX_PATH + "B1.yaml", "plain");
+        String expected = FileReader.readFile(FIX_PATH + "E2.txt");
+        String actual =  Differ.generate(FIX_PATH + "A1.yaml", FIX_PATH + "B1.yaml", "plain");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateJsonJson() throws Exception {
-        expected = FileReader.readFile(FIX_PATH + "E3.txt");
-        actual =  Differ.generate(FIX_PATH + "A1.json", FIX_PATH + "B1.json", "json");
+        String expected = FileReader.readFile(FIX_PATH + "E3.txt");
+        String actual =  Differ.generate(FIX_PATH + "A1.json", FIX_PATH + "B1.json", "json");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateYamlJson() throws Exception {
-        expected = FileReader.readFile(FIX_PATH + "E3.txt");
-        actual =  Differ.generate(FIX_PATH + "A1.yaml", FIX_PATH + "B1.yaml", "json");
+        String expected = FileReader.readFile(FIX_PATH + "E3.txt");
+        String actual =  Differ.generate(FIX_PATH + "A1.yaml", FIX_PATH + "B1.yaml", "json");
         assertEquals(expected, actual);
     }
 
