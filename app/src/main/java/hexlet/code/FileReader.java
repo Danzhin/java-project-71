468bbd3f697983d55416a9d6be084c646474cd42
dronz <dronzhin123@gmail,com>
2024-10-16 16:14:48 +0300

    update

--- app/src/main/java/hexlet/code/FileReader.java
+++ app/src/main/java/hexlet/code/FileReader.java
@@ -1,12 +1,11 @@
 package hexlet.code;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileReader {
 
-    public static String readFile(String filePath) throws IOException {
+    public static String readFile(String filePath) throws Exception {
         return Files.readString(Paths.get(filePath));
     }
 
